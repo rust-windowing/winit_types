dc3494765b3791c936025901b8c2fab007fb0208
Freya Gentz <zegentzy@protonmail.com>
2020-01-05 00:07:42 -0700

    Misc changes.
    Signed-off-by: Freya Gentz <zegentzy@protonmail.com>

--- src/error.rs
+++ src/error.rs
@@ -42,30 +42,54 @@ impl Error {
     pub fn append(&mut self, o: Error) {
         match (self, o) {
             (
-                Error { ty: ErrorType::Multiple(ref mut errs1), .. },
-                Error { ty: ErrorType::Multiple(ref mut errs2), .. },
+                Error {
+                    ty: ErrorType::Multiple(ref mut errs1),
+                    ..
+                },
+                Error {
+                    ty: ErrorType::Multiple(ref mut errs2),
+                    ..
+                },
             ) => {
                 errs1.append(errs2);
-            },
-            (err1 @ Error { ty: ErrorType::Multiple(_), .. }, err2) => {
+            }
+            (
+                err1
+                @
+                Error {
+                    ty: ErrorType::Multiple(_),
+                    ..
+                },
+                err2,
+            ) => {
                 err1.ty.append(err2);
-            },
-            (err1, mut err2 @ Error { ty: ErrorType::Multiple(_), .. }) => {
+            }
+            (
+                err1,
+                mut
+                err2
+                @
+                Error {
+                    ty: ErrorType::Multiple(_),
+                    ..
+                },
+            ) => {
                 std::mem::swap(err1, &mut err2);
                 err1.ty.append(err2);
-            },
+            }
             (err1, err2) => {
                 let mut new_err1 = lmake_error!(ErrorType::Multiple(vec![]));
                 std::mem::swap(err1, &mut new_err1);
                 err1.ty.append(new_err1);
                 err1.ty.append(err2);
-            },
+            }
         }
     }
 }
 
 /// The type of error.
 #[derive(Clone, Debug)]
+#[non_exhaustive]
 pub enum ErrorType {
     /// The operation is not supported by the backend.
     NotSupported(String),
@@ -73,6 +97,10 @@ pub enum ErrorType {
     RobustnessNotSupported,
     /// The opengl version is not supported by the backend.
     OpenGlVersionNotSupported,
+    /// Adaptive swap control is not supported.
+    AdaptiveSwapControlNotSupported,
+    /// Flush control is not supported.
+    FlushControlNotSupported,
     /// The OS cannot perform the operation.
     OsError(OsErrorWrapper),
     /// The requested config was not available.
@@ -129,6 +157,10 @@ impl fmt::Display for ErrorType {
             ErrorType::NotSupported(t) => f.pad(&format!("Operation not supported: {}", t)),
             ErrorType::RobustnessNotSupported => f.pad("Robustness not supported"),
             ErrorType::OpenGlVersionNotSupported => f.pad("OpenGL version not supported"),
+            ErrorType::AdaptiveSwapControlNotSupported => {
+                f.pad("Adaptive swap control not supported")
+            }
+            ErrorType::FlushControlNotSupported => f.pad("Flush control not supported"),
             ErrorType::NoAvailableConfig => {
                 f.pad("No available config with the requested properties")
             }
