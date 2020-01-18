029e8710db3f49cf556d7bf7f30f2c4635a8fc09
Freya Gentz <zegentzy@protonmail.com>
2020-01-17 22:30:15 -0700

    Add
    Signed-off-by: Freya Gentz <zegentzy@protonmail.com>

--- src/error.rs
+++ src/error.rs
@@ -110,6 +110,8 @@ pub enum ErrorType {
     /// The context you were using for this operation has been lost. This is
     /// generally non-recoverable.
     ContextLost,
+    /// We could not find a function ptr for the requested function.
+    FunctionDoesNotExist,
 
     /// Multiple errors happened.
     Multiple(Vec<Box<Error>>),
@@ -169,6 +171,7 @@ impl fmt::Display for ErrorType {
                 t
             )),
             ErrorType::ContextLost => f.pad("Context lost."),
+            ErrorType::FunctionDoesNotExist => f.pad("Function does not exist"),
             ErrorType::Multiple(errs) => f.pad(&format!("Multiple errors: {:?}", errs)),
         }
     }
